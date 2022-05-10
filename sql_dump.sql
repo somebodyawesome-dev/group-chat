962399ebf07bfbe8636d6260a6bb2fe212a8521a
Mohamed Bashar Touil <59267562+somebodyawesome-dev@users.noreply.github.com>
2022-05-10 23:18:19 +0100

    Update sql_dump.sql

--- sql_dump.sql
+++ sql_dump.sql
@@ -37,13 +37,6 @@ CREATE TABLE `chats` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `chats`
---
-
-/*!40000 ALTER TABLE `chats` DISABLE KEYS */;
-/*!40000 ALTER TABLE `chats` ENABLE KEYS */;
-
---
 -- Table structure for table `failed_jobs`
 --
 
@@ -64,13 +57,6 @@ CREATE TABLE `failed_jobs` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `failed_jobs`
---
-
-/*!40000 ALTER TABLE `failed_jobs` DISABLE KEYS */;
-/*!40000 ALTER TABLE `failed_jobs` ENABLE KEYS */;
-
---
 -- Table structure for table `groups`
 --
 
@@ -89,13 +75,6 @@ CREATE TABLE `groups` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `groups`
---
-
-/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
-
---
 -- Table structure for table `levels`
 --
 
@@ -109,13 +88,6 @@ CREATE TABLE `levels` (
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `levels`
---
-
-/*!40000 ALTER TABLE `levels` DISABLE KEYS */;
-/*!40000 ALTER TABLE `levels` ENABLE KEYS */;
 
 --
 -- Table structure for table `messages`
@@ -140,13 +112,6 @@ CREATE TABLE `messages` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `messages`
---
-
-/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
-
---
 -- Table structure for table `migrations`
 --
 
@@ -162,13 +127,6 @@ CREATE TABLE `migrations` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `migrations`
---
-
-/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
-
---
 -- Table structure for table `password_resets`
 --
 
@@ -182,13 +140,6 @@ CREATE TABLE `password_resets` (
   KEY `password_resets_email_index` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `password_resets`
---
-
-/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
-/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
 
 --
 -- Table structure for table `personal_access_tokens`
@@ -214,13 +165,6 @@ CREATE TABLE `personal_access_tokens` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `personal_access_tokens`
---
-
-/*!40000 ALTER TABLE `personal_access_tokens` DISABLE KEYS */;
-/*!40000 ALTER TABLE `personal_access_tokens` ENABLE KEYS */;
-
---
 -- Table structure for table `users`
 --
 
@@ -244,14 +188,6 @@ CREATE TABLE `users` (
   CONSTRAINT `users_group_foreign` FOREIGN KEY (`group`) REFERENCES `groups` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('somebodyawesome','luckynoob@yahoo.com',NULL,'$2y$10$XDDs1NYp7eZbjuOJ0DikJ.vsUp3.RkvNITWKaXfc2zfPLNKPdIcSG','etudiant',NULL,'2021-10-30 15:06:07','2022-02-04 17:40:25','FIA2-GL-3',NULL,NULL),('bachar','mmm@mm.com',NULL,'$2y$10$4YKOU6VdhqorG7ORMQtPPuFIZHHmhtj47ywH.8UqW7GuHiNZBKh2O','etudiant',NULL,'2022-01-23 12:26:45','2022-01-23 12:27:30','FIA2-GL-3','TK590BvBat4f9RUp',NULL),('some dude','somedude@someemail.com',NULL,'$2y$10$Y5G0FnaUjXMZsfM5BbuQDOW4Ebfv8EgEgYe4eLY/0svtXwKCzkQi6','etudiant',NULL,'2022-02-04 17:41:16','2022-02-04 17:41:16','FIA2-GL-3','cPaBzmRSJ289bUOt',NULL),('wejden hasni','w.hasni49@gmail.com',NULL,'$2y$10$CTYQWbQZMcgtO8g6NfS7cOnCaDmVjB5ggZXHz/uE1CWsdvnQ1SMAG','etudiant',NULL,'2022-01-15 13:52:50','2022-01-15 13:55:11','FIA2-GL-3','a2HpXAtNy07zerHD',NULL),('somebodyawful','wiow@wiow.wiow',NULL,'$2y$10$CMY.eV3Vl7j6Rf3KKtDjVuvKVVQLTtbLeSqoYC78Anxjto6pw50dm','etudiant',NULL,'2021-12-07 20:24:19','2021-12-07 20:24:19','FIA2-GL-3','Ee0sq8YThBqmX5Sx',NULL);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -262,4 +198,4 @@ INSERT INTO `users` VALUES ('somebodyawesome','luckynoob@yahoo.com',NULL,'$2y$10
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-13  0:29:36
+-- Dump completed on 2022-05-10 22:37:33
